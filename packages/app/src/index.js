75bba8406beb7fe885d34fac85bd66ddb92c6d0b
LeandroTorresSicilia <jtorressicilia@gmail.com>
2020-05-10 22:11:30 -0500

    fix: export auth components in index

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -1,3 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as RainbowFirebaseApp } from './components/App';
 export { showAppSpinner, hideAppSpinner, showAppMessage } from './actions';
