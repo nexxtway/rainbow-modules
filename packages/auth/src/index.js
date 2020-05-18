d99ec4d03a36abe880faf309216214c564676f99
HellWolf93 <drodriguezfeito@gmail.com>
2020-05-17 23:54:26 -0400

    fix: export EmailPasswordSignUpForm component

--- packages/auth/src/index.js
+++ packages/auth/src/index.js
@@ -1,2 +1,3 @@
 export { default as WhenAuthenticated } from './components/WhenAuthenticated';
 export { default as WhenNoAuthenticated } from './components/WhenNoAuthenticated';
+export { default as EmailPasswordSignUpForm } from './components/EmailPasswordSignUpForm';
