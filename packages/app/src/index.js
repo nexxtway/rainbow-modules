6fdddb037dca682cfd8a6a1bef16fafb34730244
TahimiLeonBravo <tahimi@nexxtway.com>
2020-05-25 10:49:18 -0500

    fix: import

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -1,2 +1,4 @@
 export { default as RainbowFirebaseApp } from './components/App';
+export { default as SideBarNavigation } from './components/App';
+export { default as SideBarOption } from './components/App';
 export { showAppSpinner, hideAppSpinner, showAppMessage } from './actions';
