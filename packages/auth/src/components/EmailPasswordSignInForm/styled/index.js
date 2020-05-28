02b3c8bd448eaf5f980ae0f053762418a1423624
TahimiLeonBravo <tahimi@nexxtway.com>
2020-05-27 23:37:23 -0500

    fix: login examples

--- packages/auth/src/components/EmailPasswordSignInForm/styled/index.js
+++ packages/auth/src/components/EmailPasswordSignInForm/styled/index.js
@@ -3,6 +3,7 @@ import { Button, Input } from 'react-rainbow-components';
 
 export const StyledButton = styled(Button)`
     width: 100%;
+    margin-top: 16px;
 `;
 
 export const StyledInput = styled(Input)`
