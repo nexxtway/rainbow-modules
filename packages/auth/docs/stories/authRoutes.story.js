fb36660f793a2c08a2c51be00e798988e897c271
TahimiLeonBravo <tahimi@nexxtway.com>
2020-05-27 23:48:10 -0500

    fix: hooks example sintax

--- packages/auth/docs/stories/authRoutes.story.js
+++ packages/auth/docs/stories/authRoutes.story.js
@@ -1,9 +1,38 @@
 import React, { useState } from 'react';
-import { Button, Input } from 'react-rainbow-components';
+import styled from 'styled-components';
+import { Button, Input, Card } from 'react-rainbow-components';
 import { RainbowFirebaseApp } from '@rainbow-modules/app';
 import app from '../../../../firebase';
 import WhenAuthenticated from '../../src/components/WhenAuthenticated';
 import WhenNoAuthenticated from '../../src/components/WhenNoAuthenticated';
+import RainbowLogo from './icons/rainbowLogo';
+
+const Container = styled.div.attrs((props) => props.theme.rainbow)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    background: ${(props) => props.palette.background.secondary};
+    padding: 2rem;
+`;
+
+const Title = styled.h1.attrs((props) => props.theme.rainbow)`
+    font-family: Lato Light, sans-serif !important;
+    color: ${(props) => props.palette.text.title};
+    font-size: 1.5rem;
+    margin: 1rem auto;
+    font-weight: normal;
+`;
+
+const StyledCard = styled(Card)`
+    padding: 1.5rem;
+    width: 400px;
+`;
+
+const StyledButton = styled(Button)`
+    width: 100%;
+`;
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -15,16 +44,35 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={onSubmit}>
-            <Input label="Email" value={email} onChange={(event) => setEmail(event.target.value)} />
-            <Input
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-            />
-            <Button label="Sign In" type="submit" />
-        </form>
+        <Container>
+            <RainbowLogo />
+            <Title>rainbow-modules</Title>
+            <StyledCard>
+                <form onSubmit={onSubmit}>
+                    <Input
+                        label="Email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        className="rainbow-m-vertical_small"
+                        required
+                    />
+                    <Input
+                        label="Password"
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        className="rainbow-m-vertical_small"
+                        required
+                    />
+                    <StyledButton
+                        label="Sign In"
+                        type="submit"
+                        variant="brand"
+                        className="rainbow-m-top_small"
+                    />
+                </form>
+            </StyledCard>
+        </Container>
     );
 };
 
