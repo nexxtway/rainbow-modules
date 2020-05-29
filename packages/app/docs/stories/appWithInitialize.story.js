90988bf0e2b98ec75f3a652f853af72b92bff225
David Rodriguez Feitó <drodriguezfeito@gmail.com>
2020-05-28 23:19:13 -0400

    feat: implement on RainbowFirebaseApp `initialize` prop (#44)
    * feat: implement on RainbowFirebaseApp `initialize` prop
    
    fix: #38
    
    * fix: implement proposed changes
    
    Co-authored-by: Maxx Greene <56657888+maxxgreene@users.noreply.github.com>

---
+++ packages/app/docs/stories/appWithInitialize.story.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import app from '../../../../firebase';
+import RainbowFirebaseApp from '../../src/components/App';
+import sleep from './helpers/sleep';
+
+export default {
+    title: 'App/Stories/RainbowFirebaseApp',
+    component: RainbowFirebaseApp,
+};
+
+export const appWithInitialize = () => {
+    const initialize = async () => {
+        await sleep(2000);
+    };
+
+    return (
+        <RainbowFirebaseApp app={app} initialize={initialize}>
+            <p>App is ready</p>
+        </RainbowFirebaseApp>
+    );
+};
--- packages/app/src/components/App/index.js
+++ packages/app/src/components/App/index.js
@@ -2,6 +2,7 @@ import React, { useMemo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter } from 'react-router-dom';
 import Application from 'react-rainbow-components/components/Application';
+import RenderIf from 'react-rainbow-components/components/RenderIf';
 import { FirebaseProvider } from '@rainbow-modules/firebase-hooks';
 import ReduxContainer from '../ReduxContainer';
 import I18nContainer from '../I18nContainer';
@@ -11,11 +12,12 @@ import { updateAppActions } from '../../actions';
 import getBrowserLocale from '../../helpers/getBrowserLocale';
 
 const RainbowFirebaseApp = (props) => {
-    const { app, theme, locale, translations, children, reducers } = props;
+    const { app, theme, locale, translations, children, reducers, initialize } = props;
     const firebaseContext = useMemo(() => ({ app }), [app]);
     const [isLoading, setLoading] = useState(false);
     const [isMessageVisible, setIsMessageVisible] = useState(false);
     const [messageParams, setMessageParams] = useState({});
+    const [isInitializing, setIsInitializing] = useState(true);
     const applicationLocale = locale || getBrowserLocale();
 
     useEffect(() => {
@@ -26,6 +28,18 @@ const RainbowFirebaseApp = (props) => {
         });
     }, []);
 
+    useEffect(() => {
+        async function initializeApp() {
+            if (initialize) {
+                setLoading(true);
+                await initialize();
+                setLoading(false);
+            }
+            setIsInitializing(false);
+        }
+        initializeApp();
+    }, []);
+
     return (
         <ReduxContainer reducers={reducers}>
             <FirebaseProvider value={firebaseContext}>
@@ -34,7 +48,9 @@ const RainbowFirebaseApp = (props) => {
                     messages={translations[applicationLocale]}
                 >
                     <Application theme={theme} locale={applicationLocale}>
-                        <BrowserRouter>{children}</BrowserRouter>
+                        <RenderIf isTrue={!isInitializing}>
+                            <BrowserRouter>{children}</BrowserRouter>
+                        </RenderIf>
                         <AppSpinner isLoading={isLoading} />
                         <AppMessage
                             isVisible={isMessageVisible}
@@ -62,6 +78,8 @@ RainbowFirebaseApp.propTypes = {
     translations: PropTypes.object,
     /** Object with your application Redux reducers. */
     reducers: PropTypes.object,
+    /** An async function to initialize the app. AppSpiner will be visible while this function is running. */
+    initialize: PropTypes.func,
     children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.object]),
 };
 
@@ -71,6 +89,7 @@ RainbowFirebaseApp.defaultProps = {
     translations: {},
     children: null,
     reducers: {},
+    initialize: undefined,
 };
 
 export default RainbowFirebaseApp;
