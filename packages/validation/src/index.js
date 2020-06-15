f2c3f6c172cedacba8941b161a67eb6cf9820ac8
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-06-14 23:56:45 -0400

    fix: fix index.js

--- packages/validation/src/index.js
+++ packages/validation/src/index.js
@@ -1,3 +1,4 @@
 export { default as isNumber } from './isNumber';
 export { default as isEmpty } from './isEmpty';
 export { default as isEmail } from './isEmail';
+export { default as isDate } from './isDate';
