35f51f3c7676ad7042abbae8bdbdad5ff677fbf0
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-06-15 01:51:56 -0400

    fix: fix isEmail validator

--- packages/validation/src/isEmail.js
+++ packages/validation/src/isEmail.js
@@ -1,11 +1,9 @@
 import isEmpty from './isEmpty';
 
+// eslint-disable-next-line no-useless-escape
+const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export default function isEmail(value) {
-    return (
-        !isEmpty(value) &&
-        value.match(
-            // eslint-disable-next-line no-useless-escape
-            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        ) !== null
-    );
+    if (isEmpty(value)) return false;
+    return emailRegEx.test(value);
 }
