61c5b6616ef1a648d2a903cfbdb8330df84fb178
Maxx Greene <56657888+maxxgreene@users.noreply.github.com>
2020-07-04 00:20:34 -0400

    feat: first commit listview module (#66)
    * feat: first commit listview module
    
    * Publish
    
     - @rainbow-modules/app@0.1.0
     - @rainbow-modules/auth@0.1.0
     - @rainbow-modules/firebase-hooks@0.1.0
     - @rainbow-modules/icons@0.1.0
     - @rainbow-modules/listview@0.1.0
     - @rainbow-modules/validation@0.1.0

---
+++ packages/listview/docs/stories/FirestoreTable.story.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Column } from 'react-rainbow-components';
+import { RainbowFirebaseApp } from '@rainbow-modules/app';
+import FirestoreTable from '../../src/components/FirestoreTable';
+import app from '../../../../firebase';
+
+export const basicFirestoreTableRealTime = () => {
+    return (
+        <RainbowFirebaseApp app={app}>
+            <FirestoreTable collection="books">
+                <Column field="id" header="Id" />
+                <Column field="data.name" header="Name" />
+            </FirestoreTable>
+        </RainbowFirebaseApp>
+    );
+};
+
+export const basicFirestoreTableFetchOnce = () => {
+    return (
+        <RainbowFirebaseApp app={app}>
+            <FirestoreTable collection="books" fetchOnce>
+                <Column field="id" header="Id" />
+                <Column field="data.name" header="Name" />
+            </FirestoreTable>
+        </RainbowFirebaseApp>
+    );
+};
+
+export default {
+    title: 'Listview/Stories',
+    component: FirestoreTable,
+};
---
+++ packages/listview/src/components/FirestoreTable/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+import FirestoreTableFetchOnce from './fetchOnce';
+import FireStoreTableRealTime from './realTime';
+
+const FirestoreTable = (props) => {
+    const { fetchOnce, children, ...rest } = props;
+    if (fetchOnce) {
+        return <FirestoreTableFetchOnce {...rest}>{children}</FirestoreTableFetchOnce>;
+    }
+    return <FireStoreTableRealTime {...rest}>{children}</FireStoreTableRealTime>;
+};
+
+FirestoreTable.propTypes = {
+    /** The path of the Firestore collection e.g. `/books` */
+    collection: PropTypes.string.isRequired,
+    /** It fetch the collection data once. */
+    fetchOnce: PropTypes.bool,
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.object]),
+};
+
+FirestoreTable.defaultProps = {
+    fetchOnce: false,
+    children: [],
+};
+
+export default FirestoreTable;
