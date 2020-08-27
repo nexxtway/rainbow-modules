73229d54eb7f2228e4dcd950bb3085480542b63c
rgah2107 <rgah2107@gmail.com>
2020-08-27 00:54:37 -0400

    fix: add rainbow-module logo
    fix: #86

---
+++ .storybook/managerTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+import logo from '../getting-started/assets/rainbow-modules-custom-logo.png';
+
+export default create({
+    base: 'light',
+    brandTitle: 'rainbow-modules',
+    brandUrl: './',
+    brandImage: logo,
+});
