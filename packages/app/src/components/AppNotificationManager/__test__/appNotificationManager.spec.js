b53dd74242d43edbb6a45dfdf5785044b6afca38
HellWolf93 <drodriguezfeito@gmail.com>
2020-09-03 22:39:36 -0400

    fix: remove unused import

--- packages/app/src/components/AppNotificationManager/__test__/appNotificationManager.spec.js
+++ packages/app/src/components/AppNotificationManager/__test__/appNotificationManager.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import AppNotificationManager from '..';
 
 describe('AppNotificationManager', () => {
