daa248bbb5d893a274a71184959347fd1b647000
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-09-17 13:57:19 -0400

    fix: fix scroll arrows when minDate or maxDate are undefined

--- packages/datetime/src/components/DatePickerCarousel/carouselCalendar/hooks/useScrollArrows.js
+++ packages/datetime/src/components/DatePickerCarousel/carouselCalendar/hooks/useScrollArrows.js
@@ -1,28 +1,30 @@
 import { useState, useCallback, useEffect } from 'react';
 import { compareDates, isSameDay } from '../../helpers';
+import useCalendarBounds from './useCalendarBounds';
 
 export default function useScrollArrows({ minDate, maxDate, visibleDates, moveLeft, moveRight }) {
+    const { minCalendarDate, maxCalendarDate } = useCalendarBounds({ minDate, maxDate });
     const [disableScrollLeft, setDisableScrollLeft] = useState(false);
     const [disableScrollRight, setDisableScrollRight] = useState(false);
 
     const scrollLeftClick = useCallback(() => {
         const nextStartDate = moveLeft(1);
-        setDisableScrollLeft(isSameDay(nextStartDate, minDate));
-    }, [moveLeft, minDate]);
+        setDisableScrollLeft(isSameDay(nextStartDate, minCalendarDate));
+    }, [moveLeft, minCalendarDate]);
 
     const scrollRightClick = useCallback(() => {
         const nextStartDate = moveRight(1);
-        setDisableScrollRight(isSameDay(nextStartDate, maxDate));
-    }, [moveRight, maxDate]);
+        setDisableScrollRight(isSameDay(nextStartDate, maxCalendarDate));
+    }, [moveRight, maxCalendarDate]);
 
     useEffect(() => {
         if (visibleDates.length > 0) {
-            setDisableScrollLeft(compareDates(visibleDates[0], minDate) <= 0);
+            setDisableScrollLeft(compareDates(visibleDates[0], minCalendarDate) <= 0);
             setDisableScrollRight(
-                compareDates(visibleDates[visibleDates.length - 1], maxDate) >= 0,
+                compareDates(visibleDates[visibleDates.length - 1], maxCalendarDate) >= 0,
             );
         }
-    }, [maxDate, minDate, visibleDates]);
+    }, [minCalendarDate, maxCalendarDate, visibleDates]);
 
     return {
         disableScrollLeft,
