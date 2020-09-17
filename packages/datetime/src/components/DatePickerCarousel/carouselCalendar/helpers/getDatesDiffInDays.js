c946bf8482d9adebeca73c5c61e25f453628e15d
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-09-17 19:38:05 -0400

    fix: remove unused code

--- packages/datetime/src/components/DatePickerCarousel/carouselCalendar/helpers/getDatesDiffInDays.js
+++
@@ -1,5 +0,0 @@
-export default function getDatesDiffInDays(minDate, maxDate) {
-    const date1 = new Date(minDate).setHours(0, 0, 0, 0);
-    const date2 = new Date(maxDate).setHours(0, 0, 0, 0);
-    return Math.ceil((Math.abs(date2 - date1) / 1000) * 60 * 60 * 24);
-}
--- packages/datetime/src/components/DatePickerCarousel/helpers/addMonths.js
+++
@@ -1,5 +0,0 @@
-export default function addMonths(date, months) {
-    const clone = new Date(date);
-    clone.setMonth(date.getMonth() + months);
-    return clone;
-}
