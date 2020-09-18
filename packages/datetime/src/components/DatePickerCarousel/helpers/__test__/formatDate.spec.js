eeebf3b1e3017ae5ec99bbf28b4eb1fc22a23c4f
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-09-17 23:00:45 -0400

    test: fix failing tests

--- packages/datetime/src/components/DatePickerCarousel/helpers/__test__/formatDate.spec.js
+++ packages/datetime/src/components/DatePickerCarousel/helpers/__test__/formatDate.spec.js
@@ -12,9 +12,9 @@ describe('formatDate', () => {
         expect(formatDate('wrong date')).toBe('');
     });
     it('should return the right formatted date', () => {
-        expect(formatDate(new Date(2019, 3, 24))).toBe('04/24/2019');
-        expect(formatDate(new Date('04/24/2019'))).toBe('04/24/2019');
-        expect(formatDate('04/24/2019')).toBe('04/24/2019');
+        expect(formatDate(new Date(2019, 3, 24))).toBe('April 24, 2019');
+        expect(formatDate(new Date('04/24/2019'))).toBe('April 24, 2019');
+        expect(formatDate('04/24/2019')).toBe('April 24, 2019');
     });
     it('should return the right formatted date when formatStyle large', () => {
         expect(formatDate(new Date(2019, 3, 24), 'large')).toBe('Wednesday, 04/24/2019');
@@ -27,8 +27,8 @@ describe('formatDate', () => {
         expect(formatDate('04/24/2019', 'small')).toBe('4/24/19');
     });
     it('should return the right formatted date when formatStyle is wrong', () => {
-        expect(formatDate(new Date(2019, 3, 24), 'normal')).toBe('04/24/2019');
-        expect(formatDate(new Date('04/24/2019'), 'big')).toBe('04/24/2019');
-        expect(formatDate('04/24/2019', 'short')).toBe('04/24/2019');
+        expect(formatDate(new Date(2019, 3, 24), 'normal')).toBe('April 24, 2019');
+        expect(formatDate(new Date('04/24/2019'), 'big')).toBe('April 24, 2019');
+        expect(formatDate('04/24/2019', 'short')).toBe('April 24, 2019');
     });
 });
