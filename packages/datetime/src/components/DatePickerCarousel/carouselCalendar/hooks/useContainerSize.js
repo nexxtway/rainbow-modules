6a1e229cbc96ab59543d76630b1d303a083e14ab
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-09-17 20:19:49 -0400

    fix: fix useContainerSize hook

--- packages/datetime/src/components/DatePickerCarousel/carouselCalendar/hooks/useContainerSize.js
+++ packages/datetime/src/components/DatePickerCarousel/carouselCalendar/hooks/useContainerSize.js
@@ -1,13 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 export default function useContainerSize({ containerRef }) {
     const [containerSize, setContainerSize] = useState(0);
 
+    const observer = useRef(
+        new ResizeObserver((entries) => {
+            const { width } = entries[0].contentRect;
+            setContainerSize(width);
+        }),
+    );
+
     useEffect(() => {
+        const currentObserver = observer.current;
         if (containerRef.current) {
-            setContainerSize(containerRef.current.clientWidth);
+            currentObserver.observe(containerRef.current);
         }
-    }, [containerRef]);
+
+        return () => {
+            currentObserver.unobserve();
+        };
+    }, [containerRef, observer]);
 
     return containerSize;
 }
