37fb36c1a24945fbaa700888b9732df4eb5c8a90
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-09-27 23:56:53 -0400

    test: fix tests

--- cypress/integration/datetime/datePickerCarousel.js
+++ cypress/integration/datetime/datePickerCarousel.js
@@ -7,6 +7,7 @@ const DATE_PICKER_MODAL = `${DATE_PICKER_CAROUSEL}_modal`;
 
 describe('DatePickerCarousel', () => {
     beforeEach(() => {
+        cy.viewport(1024, 768);
         cy.visit(FORM_URL);
     });
     it('it should open modal when click on input label', () => {
