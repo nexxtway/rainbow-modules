5c300d3fa3eb24f39211e2d52b8882b3e8aeaaac
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-10-04 21:23:11 -0400

    test: fix tests

--- cypress/integration/datetime/datePickerCarousel.js
+++ cypress/integration/datetime/datePickerCarousel.js
@@ -1,7 +1,7 @@
 const FORM_URL = '/iframe.html?id=modules-datetime-stories-datepickercarousel--with-date-bounds';
 const DATE_PICKER_CAROUSEL = '#date-picker-carousel-1';
 const PICKER_LABEL = `${DATE_PICKER_CAROUSEL} [data-cy=carousel-input__label-button]`;
-const CALENDAR_DAY_CARD = `${DATE_PICKER_CAROUSEL} [data-cy=carousel-calendar__day-card]`;
+const CALENDAR_DAY_CARD = `${DATE_PICKER_CAROUSEL} [data-cy=carousel-calendar__day-cards-container] > [data-cy=carousel-calendar__day-card]`;
 const CALENDAR_SCROLL_BUTTON = `${DATE_PICKER_CAROUSEL} [data-id=button-icon-element]`;
 const DATE_PICKER_MODAL = `${DATE_PICKER_CAROUSEL}_modal`;
 
@@ -9,20 +9,19 @@ describe('DatePickerCarousel', () => {
     beforeEach(() => {
         cy.viewport(1024, 768);
         cy.visit(FORM_URL);
-        cy.reload(true);
     });
     it('it should open modal when click on input label', () => {
         cy.get(PICKER_LABEL).click();
         cy.get(DATE_PICKER_MODAL).should('exist');
     });
     it('should set the right value when select a date', () => {
-        cy.get(CALENDAR_DAY_CARD).contains('12').click();
-        cy.get(PICKER_LABEL).contains('September 12, 2020');
+        cy.get(CALENDAR_DAY_CARD).eq(6).click();
+        cy.get(PICKER_LABEL).should('contain', 'September 18, 2020');
     });
     it('should select date when ENTER_KEY is pressed', () => {
-        cy.get(CALENDAR_DAY_CARD).eq(8).type('{enter}');
-        cy.get(CALENDAR_DAY_CARD).eq(8).should('be.focused');
-        cy.get(CALENDAR_DAY_CARD).eq(8).should('have.attr', 'data-selected', 'true');
+        cy.get(CALENDAR_DAY_CARD).eq(5).type('{enter}');
+        cy.get(CALENDAR_DAY_CARD).eq(5).should('be.focused');
+        cy.get(CALENDAR_DAY_CARD).eq(5).should('have.attr', 'data-selected', 'true');
     });
     it('should disable scroll left button when minDate is visible', () => {
         cy.get(CALENDAR_SCROLL_BUTTON).eq(0).should('not.be.disabled');
@@ -45,21 +44,15 @@ describe('DatePickerCarousel', () => {
         cy.get(CALENDAR_DAY_CARD).eq(2).should('be.focused');
     });
     it('should focus previous date and scroll left when first visible date is focused and press ARROW_LEFT key', () => {
-        cy.get(CALENDAR_DAY_CARD).contains('11').should('not.exist');
-        cy.get(CALENDAR_DAY_CARD).contains('12').type('{leftArrow}');
-        cy.get(CALENDAR_DAY_CARD).contains('11').should('exist');
-        // cy.get(CALENDAR_DAY_CARD).eq(0).contains('12');
-        // cy.get(CALENDAR_DAY_CARD).eq(0).type('{leftArrow}');
-        // cy.get(CALENDAR_DAY_CARD).eq(0).should('be.focused');
-        // cy.get(CALENDAR_DAY_CARD).eq(0).contains('11');
+        cy.get(CALENDAR_DAY_CARD).first().should('contain', '12');
+        cy.get(CALENDAR_DAY_CARD).first().type('{leftArrow}');
+        cy.get(CALENDAR_DAY_CARD).first().should('be.focused');
+        cy.get(CALENDAR_DAY_CARD).first().should('contain', '11');
     });
     it('should focus next date and scroll right when latest visible date is focused and press ARROW_RIGHT key', () => {
-        cy.get(CALENDAR_DAY_CARD).contains('25').should('not.exist');
-        cy.get(CALENDAR_DAY_CARD).contains('24').type('{rightArrow}');
-        cy.get(CALENDAR_DAY_CARD).contains('25').should('exist');
-        // cy.get(CALENDAR_DAY_CARD).eq(12).children('span').eq(1).contains('24');
-        // cy.get(CALENDAR_DAY_CARD).eq(12).type('{rightArrow}');
-        // cy.get(CALENDAR_DAY_CARD).eq(12).should('be.focused');
-        // cy.get(CALENDAR_DAY_CARD).eq(12).children('span').eq(1).contains('25');
+        cy.get(CALENDAR_DAY_CARD).last().should('contain', '24');
+        cy.get(CALENDAR_DAY_CARD).last().type('{rightArrow}');
+        cy.get(CALENDAR_DAY_CARD).last().should('be.focused');
+        cy.get(CALENDAR_DAY_CARD).last().should('contain', '25');
     });
 });
