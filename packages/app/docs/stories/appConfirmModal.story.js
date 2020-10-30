17b1c92cedca9272ceef8ff4dc93fdc4db6de3ec
amontalvof <amontalvof92@gmail.com>
2020-10-30 16:21:30 -0600

    docs: removed confirmmodal in appconfirmmodal.story.js

--- packages/app/docs/stories/appConfirmModal.story.js
+++ packages/app/docs/stories/appConfirmModal.story.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import { Button, Card as RainbowCard, ButtonIcon } from 'react-rainbow-components';
 import { RainbowLogo, Trash } from '@rainbow-modules/icons';
 import RainbowFirebaseApp from '../../src/components/App';
-import ConfirmModal from '../../src/components/ConfirmModal';
 import { confirmModal, showAppMessage } from '../../src/actions';
 
 const TrashIcon = styled(Trash)`
