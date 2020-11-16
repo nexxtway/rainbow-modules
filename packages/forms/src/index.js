736184d907cee6fcaf9f00caf4eadfa4b423903f
amontalvof <amontalvof92@gmail.com>
2020-11-15 18:26:14 -0600

    feat: implement floatingsearch module

--- packages/forms/src/index.js
+++ packages/forms/src/index.js
@@ -1,2 +1,3 @@
 export { default as UniversalPicker } from './components/UniversalPicker';
 export { default as UniversalPickerOption } from './components/UniversalPickerOption';
+export { default as FloatingSearch } from './components/FloatingSearch';
