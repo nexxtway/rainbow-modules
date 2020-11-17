385bacc6f44b371e554bef954d5a42d14b46d6f7
Andy Montalvo <59586600+amontalvof@users.noreply.github.com>
2020-11-17 16:11:31 -0600

    fix: move floatingsearchbuttonicon to listview (#205)
    * fix: moved floatingsearchbuttonicon to listview
    
    * fix: deleted type prop of floatingsearchbuttonicon
    
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

--- packages/listview/src/index.js
+++ packages/listview/src/index.js
@@ -3,3 +3,5 @@ export { default as FirestoreTable } from './components/FirestoreTable';
 export { default as ColoredStatusColumn } from './components/ColoredStatusColumn';
 export { default as BatchActionsBar } from './components/BatchActionsBar';
 export { default as NavigationButtonColumn } from './components/NavigationButtonColumn';
+export { default as FloatingSearch } from './components/FloatingSearch';
+export { default as FloatingSearchButtonIcon } from './components/FloatingSearchButtonIcon';
