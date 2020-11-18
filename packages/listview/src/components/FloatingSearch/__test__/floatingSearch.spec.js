7c95c6a9b7720529bbe8ddbf226691140d13e7b0
amontalvof <amontalvof92@gmail.com>
2020-11-18 16:41:08 -0600

    fix: added tests to floatingsearch and floatingsearchbuttonicon

---
+++ packages/listview/src/components/FloatingSearch/__test__/floatingSearch.spec.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Search from '../search';
+
+describe('<FloatingSearch />', () => {
+    it('should render an input type search and a button', () => {
+        const component = mount(<Search />);
+        const input = component.find('input');
+        const button = component.find('button');
+        expect(input.prop('type')).toBe('search');
+        expect(button.exists()).toBe(true);
+    });
+
+    it('should render an input with value = a and 2 buttons', () => {
+        const component = mount(<Search value="a" />);
+        const input = component.find('input');
+        const buttons = component.find('button');
+        expect(input.prop('value')).toBe('a');
+        expect(buttons.length).toBe(2);
+    });
+
+    it('should call onChage with value = b', () => {
+        const onChangeMockFn = jest.fn();
+        const component = mount(<Search value="a" onChange={onChangeMockFn} />);
+        const input = component.find('input');
+        input.simulate('change', { target: { value: 'b' } });
+        expect(onChangeMockFn.mock.calls.length).toBe(1);
+        expect(onChangeMockFn).toBeCalledWith('b');
+    });
+
+    it('should call onRequestClose', () => {
+        const onRequestCloseMockFn = jest.fn();
+        const component = mount(<Search value="a" onRequestClose={onRequestCloseMockFn} />);
+        const buttons = component.find('button');
+        buttons.at(1).simulate('click');
+        expect(onRequestCloseMockFn.mock.calls.length).toBe(1);
+    });
+
+    it('should call onChage with empty string to clear the input', () => {
+        const onChangeMockFn = jest.fn();
+        const component = mount(<Search value="a" onChange={onChangeMockFn} />);
+        const buttons = component.find('button');
+        buttons.at(0).simulate('mouseDown');
+        expect(onChangeMockFn.mock.calls.length).toBe(1);
+        expect(onChangeMockFn).toBeCalledWith('');
+    });
+});
---
+++ packages/listview/src/components/FloatingSearchButtonIcon/__test__/floatingSearchButtonIcon.spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import FloatingSearchButtonIcon from '../index';
+import FloatingSearch from '../../FloatingSearch';
+
+jest.mock('../../FloatingSearch', () =>
+    jest.fn(({ ...props }) => <div {...props}>hello world</div>),
+);
+
+describe('<FloatingSearchButtonIcon />', () => {
+    it('should render a button and not render FloatingSearch', () => {
+        const component = mount(<FloatingSearchButtonIcon />);
+        const button = component.find('button');
+        expect(button.exists()).toBe(true);
+        expect(component.find(FloatingSearch).prop('isVisible')).toBe(false);
+    });
+
+    it('should render a FloatingSearch when click on button', () => {
+        const component = mount(<FloatingSearchButtonIcon />);
+        const button = component.find('button');
+        button.simulate('click');
+        expect(component.find(FloatingSearch).prop('isVisible')).toBe(true);
+    });
+});
