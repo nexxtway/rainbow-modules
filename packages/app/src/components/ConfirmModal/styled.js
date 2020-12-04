5216a1412714dee9f25eb784e59fd601bfe09bc9
Tahimi <tahimileon@gmail.com>
2020-12-04 08:57:20 -0600

    feat: add children confirm modal (#225)
    * feat: add children confirm modal
    
    * fix: add RenderIf

--- packages/app/src/components/ConfirmModal/styled.js
+++ packages/app/src/components/ConfirmModal/styled.js
@@ -3,22 +3,11 @@ import { Button as RainbowButton } from 'react-rainbow-components';
 
 export const Container = styled.div`
     display: flex;
-    flex-direction: ${(props) => props.direction};
 `;
 
 export const IconContainer = styled.div`
-    margin: 1.32rem 0 1rem 1rem;
-`;
-
-export const Header = styled.div`
-    font-size: 22px;
-    font-weight: 900;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.27;
-    letter-spacing: normal;
-    color: ${(props) => props.theme.rainbow.palette.text.title};
-    padding: 2rem 2rem 1rem 1rem;
+    margin-left: 24px;
+    padding: 0;
 `;
 
 export const Question = styled.div`
@@ -29,7 +18,7 @@ export const Question = styled.div`
     line-height: 1.56;
     letter-spacing: normal;
     color: ${(props) => props.theme.rainbow.palette.text.main};
-    padding: 0 2rem 0.5rem 1rem;
+    padding: 0 1rem 0.5rem 1rem;
 `;
 
 export const ButtonsContainer = styled.div`
