cd6debc71bd65bdd60d1b62c82983adeb561f813
Jose Leandro Torres <jtorressicilia@gmail.com>
2020-12-17 14:38:17 -0600

    feat: add tab amounts in GlobalSearch (#245)

--- packages/search/src/components/GlobalSearch/tabs.js
+++ packages/search/src/components/GlobalSearch/tabs.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Tab } from 'react-rainbow-components';
 
+const TabLabel = ({ totalHits, label }) => {
+    if (typeof totalHits === 'number') {
+        return <span>{`${label} ${totalHits}`}</span>;
+    }
+    return label;
+};
+
 const Tabs = (props) => {
     const { results } = props;
     return Object.keys(results).map((entityName) => {
-        return <Tab key={entityName} label={entityName} name={entityName} id={entityName} />;
+        const { totalHits } = results[entityName];
+        return (
+            <Tab
+                key={entityName}
+                label={<TabLabel totalHits={totalHits} label={entityName} />}
+                name={entityName}
+                id={entityName}
+            />
+        );
     });
 };
 
