3143a7ee20eb66747003d3e211ebede1b3778c7a
amontalvof <amontalvof92@gmail.com>
2021-01-10 09:21:43 -0600

    fix: removed crash svg

--- packages/app/src/components/Crashes/errorMessage.js
+++ packages/app/src/components/Crashes/errorMessage.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-rainbow-components';
 import { ErrorContainer, ErrorText, Title, SecondTitle } from './styled';
-import Crash from './crash.svg';
+import CrashImg from './crashImg';
 
 const ErrorMessage = ({ errorTrace }) => {
     return (
         <ErrorContainer>
-            <img src={Crash} alt="crash" />
+            <CrashImg />
             <Title>We&apos;ve run into an issue.</Title>
             <SecondTitle>We apologize for the inconvenience.</SecondTitle>
             <Button variant="base">Go Back</Button>
