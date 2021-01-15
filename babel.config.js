860f146b316b4d0a9fdfcb708af1679a5a24a209
HellWolf93 <drodriguezfeito@gmail.com>
2021-01-15 01:13:45 -0500

    chore: upgrade Storybook to v6

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     compact: true,
-    presets: ['@babel/preset-env', '@babel/preset-react'],
+    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
     plugins: ['babel-plugin-styled-components', '@babel/plugin-transform-runtime'],
 };
