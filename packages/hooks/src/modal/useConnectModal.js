3a8a69b50d5c81222b10d7d13cbae09b50558894
Reinier Guerra <reinier.guerra@salesforce.com>
2021-01-19 19:14:37 -0600

    docs: add for useConnectModal & useOpenModal (#300)
    * docs: add for useConnectModal & useOpenModal
    
    * fix: dep

--- packages/hooks/src/modal/useConnectModal.js
+++ packages/hooks/src/modal/useConnectModal.js
@@ -3,9 +3,7 @@ import store, { initialDefaultProps } from './store';
 
 const useConnectModal = (modalId, defaultProps = {}) => {
     const [props, setProps] = useState({ ...initialDefaultProps, ...defaultProps });
-    if (!store.has(modalId)) {
-        store.set(modalId, [defaultProps, setProps]);
-    }
+    store.set(modalId, [defaultProps, setProps]);
     return props;
 };
 
