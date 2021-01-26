16b0420480f3743d811c554194262c2a2bb6bcb8
HellWolf93 <drodriguezfeito@gmail.com>
2021-01-26 01:02:28 -0500

    docs: document firebase-hooks

---
+++ packages/firebase-hooks/docs/stories/useDocOnce.story.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Table, Column } from 'react-rainbow-components';
+import { RainbowFirebaseApp } from '@rainbow-modules/app';
+import app from '../../../../firebase';
+import useDocOnce from '../../src/firestore/useDocOnce';
+
+const Book = () => {
+    const [data, isLoading] = useDocOnce({
+        path: '/books/RoguXc30cdJvA4J7jWDc',
+    });
+    const tableData = isLoading ? [] : [data];
+    return (
+        <Table keyField="id" data={tableData} isLoading={isLoading} variant="listview">
+            <Column field="data.name" header="Name" />
+            <Column field="data.author" header="Author" />
+        </Table>
+    );
+};
+
+export const UseDocOnce = () => {
+    return (
+        <RainbowFirebaseApp app={app}>
+            <Book />
+        </RainbowFirebaseApp>
+    );
+};
