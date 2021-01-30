b5c1fcfa02b72ab8a2e4a617d3b0959eb5006ae9
Wilder Gonzalez Diaz <wildergd@gmail.com>
2021-01-29 22:25:09 -0500

    test: fix tests

--- packages/listview/src/components/FloatingSearchButtonIcon/__test__/floatingSearchButtonIcon.spec.js
+++ packages/listview/src/components/FloatingSearchButtonIcon/__test__/floatingSearchButtonIcon.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { Button } from 'react-rainbow-components';
+import { Button, ButtonIcon } from 'react-rainbow-components';
 import FloatingSearchButtonIcon from '..';
 import FloatingSearch from '../../FloatingSearch';
 
@@ -11,14 +11,20 @@ jest.mock('../../FloatingSearch', () =>
 describe('<FloatingSearchButtonIcon />', () => {
     it('should render a button and not render FloatingSearch', () => {
         const component = mount(<FloatingSearchButtonIcon />);
-        const button = component.find(Button);
+        const button = component.find(ButtonIcon);
         expect(button.exists()).toBe(true);
         expect(component.find(FloatingSearch).prop('isVisible')).toBe(false);
     });
 
+    it('should render a Button with Filter as label when click on button', () => {
+        const component = mount(<FloatingSearchButtonIcon />);
+        const button = component.find(ButtonIcon);
+        button.simulate('click');
+        expect(component.find(Button).text()).toBe('Filter');
+    });
     it('should render a FloatingSearch when click on button', () => {
         const component = mount(<FloatingSearchButtonIcon />);
-        const button = component.find(Button);
+        const button = component.find(ButtonIcon);
         button.simulate('click');
         expect(component.find(FloatingSearch).prop('isVisible')).toBe(true);
     });
