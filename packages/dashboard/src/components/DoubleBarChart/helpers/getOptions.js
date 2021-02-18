a5b3062e61d0577e77755bf8091d6229bdc28c1b
Wilder Gonzalez Diaz <wildergd@gmail.com>
2021-02-18 12:47:08 -0500

    fix: fix chart legend align

--- packages/dashboard/src/components/DoubleBarChart/helpers/getOptions.js
+++ packages/dashboard/src/components/DoubleBarChart/helpers/getOptions.js
@@ -5,6 +5,7 @@ export default function getOptions(options) {
         legend: {
             display: true,
             position: 'top',
+            align: 'start',
         },
         scales: {
             xAxes: [
