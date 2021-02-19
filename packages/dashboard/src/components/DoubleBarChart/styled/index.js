7e498b01f438fa6670331237180e349fd6ccef5e
Wilder González Díaz <wildergd@gmail.com>
2021-02-19 10:17:01 -0500

    fix: force disable legend in `DoubleBarChart` component (#323)
    * fix: force disable legend in chart.js options
    
    * fix: remove legend and enabled chart.js legend
    
    * test: remove legend related tests
    
    * fix: lint issues
    
    * fix: fix chart legend align
    
    * Update getOptions.js

--- packages/dashboard/src/components/DoubleBarChart/styled/index.js
+++ packages/dashboard/src/components/DoubleBarChart/styled/index.js
@@ -1,36 +1,8 @@
+/* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
-const StyledContainer = styled.div`
+export const StyledContainer = styled.div`
     position: relative;
     width: 100%;
     height: 100%;
 `;
-
-const StyledUl = styled.ul`
-    display: flex;
-    list-style: none;
-`;
-
-const StyledLi = styled.li`
-    display: flex;
-    margin-right: 1em;
-`;
-
-const Bullet = styled.span`
-    position: relative;
-    display: inline-block;
-    width: 16px;
-    height: 16px;
-    border-radius: 50%;
-    margin-right: 10px;
-    background-color: ${(props) => props.color};
-`;
-
-const StyledSpan = styled.span`
-    text-transform: uppercase;
-    line-height: 16px;
-    font-weight: bold;
-    color: ${(props) => props.color};
-`;
-
-export { StyledContainer, StyledUl, StyledLi, Bullet, StyledSpan };
