a0b3f90f3a70b78bad249c73099c725c2632d037
Wilder Gonzalez Diaz <wildergd@gmail.com>
2021-03-01 12:49:29 -0500

    fix: validation error

--- packages/validation/src/__test__/isFirestorePath.spec.js
+++ packages/validation/src/__test__/isFirestorePath.spec.js
@@ -1,24 +1,24 @@
 import isFirestorePath from '../isFirestorePath';
 
 describe('isFirestorePath', () => {
-    // it('shoud return true', () => {
-    //     [
-    //         '/users',
-    //         '/users/marie',
-    //         '/users/{username}/addresses',
-    //         '/users/{username}/addresses/{addressId}',
-    //         '/users/{username}/addresses/home',
-    //         '/{collectionName}/{documentId}',
-    //         'users',
-    //         'users/{username}',
-    //         'users/{username}/addresses',
-    //         'users/{username}/addresses/{addressId}',
-    //         'users/{username}/addresses/home',
-    //         '{collectionName}/{documentId}',
-    //     ].forEach((path) => {
-    //         expect(isFirestorePath(path)).toBe(true);
-    //     });
-    // });
+    it('shoud return true', () => {
+        [
+            '/users',
+            '/users/marie',
+            '/users/{username}/addresses',
+            '/users/{username}/addresses/{addressId}',
+            '/users/{username}/addresses/home',
+            '/{collectionName}/{documentId}',
+            'users',
+            'users/{username}',
+            'users/{username}/addresses',
+            'users/{username}/addresses/{addressId}',
+            'users/{username}/addresses/home',
+            '{collectionName}/{documentId}',
+        ].forEach((path) => {
+            expect(isFirestorePath(path)).toBe(true);
+        });
+    });
     it('shoud return false', () => {
         [
             'users/[username]/addresses',
