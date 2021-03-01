5fa8d137a9ba9995b8a6e2d101b4f77faf0949b2
Wilder Gonzalez Diaz <wildergd@gmail.com>
2021-03-01 11:55:15 -0500

    fix: remove individual regex validation

--- packages/validation/src/isFirestorePath.js
+++ packages/validation/src/isFirestorePath.js
@@ -1,15 +1,17 @@
 import isEmpty from './isEmpty';
 import splitPath from './helpers/splitPath';
 
-const isIdentifier = (str) => /^[^{|^}][\w|\-|+|_|%]{1,1500}$/gi.test(str);
-const isParameter = (str) => /^\{[\w|\-|+|_|%]{1,1500}\}$/gi.test(str);
+const pathElementValidationRegex = [
+    /^[^{|^}][\w|\-|+|_|%]{1,1500}$/gi,
+    /^\{[\w|\-|+|_|%]{1,1500}\}$/gi,
+];
 const isValidPath = (str) => /^\/?(?:[^/]{1,1500})(?:\/[^/]{1,1500})*$/gi.test(str);
 
 export default function isFirestorePath(value) {
     if (isEmpty(value)) return false;
     if (isValidPath(value)) {
         const parts = splitPath(value);
-        return parts.every((el) => isIdentifier(el) || isParameter(el));
+        return parts.every((el) => pathElementValidationRegex.some((regex) => regex.test(el)));
     }
     return false;
 }
