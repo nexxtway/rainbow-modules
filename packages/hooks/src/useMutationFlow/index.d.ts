e8e6b25cd6316c6fa53313933be19bd98a24a193
LeandroTorresSicilia <jtorressicilia@gmail.com>
2021-03-14 11:03:07 -0600

    feat: support messages in useMutationFlow hook

--- packages/hooks/src/useMutationFlow/index.d.ts
+++ packages/hooks/src/useMutationFlow/index.d.ts
@@ -1,9 +1,17 @@
+type feedbackType = 'notification' | 'message';
+
+interface Type {
+    success?: feedbackType;
+    error?: feedbackType;
+}
+
 export interface MutationOptions {
-    mutation?: () => void;
-    successMessage?: string;
-    errorMessage?: string;
+    mutation?: (unknow) => void;
+    successMessage?: string | ((unknow) => void);
+    errorMessage?: string | ((unknow) => void);
     onSuccess?: () => void;
     onError?: () => void;
+    type?: feedbackType | Type;
 }
 
 export default function (opts: MutationOptions): { mutate: () => void };
