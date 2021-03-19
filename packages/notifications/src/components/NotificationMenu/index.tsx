eb99399a2ffe65d9182431dd190eeef6d7ec4091
Wilder González Díaz <wildergd@gmail.com>
2021-03-19 19:14:56 -0400

    fix: minor issue (#362)

--- packages/notifications/src/components/NotificationMenu/index.tsx
+++ packages/notifications/src/components/NotificationMenu/index.tsx
@@ -16,86 +16,73 @@ import positionResolver from './helpers/positionResolver';
 const NotificationMenu: React.ForwardRefExoticComponent<NotificationMenuProps> = React.forwardRef<
     NotificationMenuHandles,
     NotificationMenuProps
->(
-    (
-        {
-            label,
-            footer,
-            unreads,
-            isLoading,
-            notifications,
-            onClick,
-            className,
-            style,
-        }: NotificationMenuProps,
-        ref,
-    ) => {
-        const [isOpen, setIsOpen] = useState(false);
-        const buttonRef = useRef<ButtonIconHandle>();
-        const dropdownRef = useRef<HTMLElement>(null);
-        const inProgress = notifications?.some((value) => value.status === 'inProgress');
-        const icon = (
-            <BadgeOverlay overlap="circle" isHidden={!unreads}>
-                <Bell />
-            </BadgeOverlay>
-        );
+>((props: NotificationMenuProps, ref) => {
+    const { label, footer, unreads, isLoading, notifications, onClick, className, style } = props;
+    const [isOpen, setIsOpen] = useState(false);
+    const buttonRef = useRef<ButtonIconHandle>();
+    const dropdownRef = useRef<HTMLElement>(null);
+    const inProgress = notifications?.some((value) => value.status === 'inProgress');
+    const icon = (
+        <BadgeOverlay overlap="circle" isHidden={!unreads}>
+            <Bell />
+        </BadgeOverlay>
+    );
 
-        useOutsideClick(
-            () => buttonRef.current?.htmlElementRef.current,
-            (event) => {
-                if (dropdownRef.current?.contains(event.target as Node)) return;
-                setIsOpen(false);
-            },
-            isOpen,
-        );
+    useOutsideClick(
+        () => buttonRef.current?.htmlElementRef.current,
+        (event) => {
+            if (dropdownRef.current?.contains(event.target as Node)) return;
+            setIsOpen(false);
+        },
+        isOpen,
+    );
 
-        useImperativeHandle(ref, () => ({
-            close: () => {
-                setIsOpen(false);
-            },
-        }));
+    useImperativeHandle(ref, () => ({
+        close: () => {
+            setIsOpen(false);
+        },
+    }));
 
-        const handleButtonClick = () => {
-            setIsOpen(!isOpen);
-        };
+    const handleButtonClick = () => {
+        setIsOpen(!isOpen);
+    };
 
-        const handleItemClick = (notification: NotificationInterface) => {
-            setIsOpen(!isOpen);
-            if (onClick) {
-                onClick(notification);
-            }
-        };
+    const handleItemClick = (notification: NotificationInterface) => {
+        setIsOpen(!isOpen);
+        if (onClick) {
+            onClick(notification);
+        }
+    };
 
-        return (
-            <NotificationMenuContainer className={className} style={style}>
-                <RenderIf isTrue={inProgress}>
-                    <StyledSpinner type="arc" variant="brand">
-                        <ButtonIcon icon={icon} onClick={handleButtonClick} ref={buttonRef} />
-                    </StyledSpinner>
-                </RenderIf>
-                <RenderIf isTrue={!inProgress}>
+    return (
+        <NotificationMenuContainer className={className} style={style}>
+            <RenderIf isTrue={inProgress}>
+                <StyledSpinner type="arc" variant="brand">
                     <ButtonIcon icon={icon} onClick={handleButtonClick} ref={buttonRef} />
-                </RenderIf>
-                <InternalOverlay
-                    isVisible={isOpen}
-                    triggerElementRef={buttonRef.current?.htmlElementRef}
-                    positionResolver={positionResolver}
-                    render={() => (
-                        <Dropdown
-                            label={label}
-                            footer={footer}
-                            notifications={notifications}
-                            unreads={unreads}
-                            isLoading={isLoading}
-                            onClick={handleItemClick}
-                            ref={dropdownRef}
-                        />
-                    )}
-                />
-            </NotificationMenuContainer>
-        );
-    },
-);
+                </StyledSpinner>
+            </RenderIf>
+            <RenderIf isTrue={!inProgress}>
+                <ButtonIcon icon={icon} onClick={handleButtonClick} ref={buttonRef} />
+            </RenderIf>
+            <InternalOverlay
+                isVisible={isOpen}
+                triggerElementRef={buttonRef.current?.htmlElementRef}
+                positionResolver={positionResolver}
+                render={() => (
+                    <Dropdown
+                        label={label}
+                        footer={footer}
+                        notifications={notifications}
+                        unreads={unreads}
+                        isLoading={isLoading}
+                        onClick={handleItemClick}
+                        ref={dropdownRef}
+                    />
+                )}
+            />
+        </NotificationMenuContainer>
+    );
+});
 
 NotificationMenu.defaultProps = {
     label: 'Notifications',
