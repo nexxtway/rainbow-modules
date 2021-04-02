422ae2fe7b3c5ad8f9fbf74b93c47072fccc6ec1
HellWolf93 <drodriguezfeito@gmail.com>
2021-04-02 18:59:52 -0400

    fix: remove headings and code rows data

--- packages/listview/docs/stories/MarkdownColumn.story.js
+++ packages/listview/docs/stories/MarkdownColumn.story.js
@@ -4,12 +4,6 @@ import MarkdownColumn from '../../src/components/MarkdownColumn';
 
 const initialData = [
     {
-        customer: 'Headings',
-        markdown:
-            '# # Heading level 1\n ## ## Heading level 2\n ### ### Heading level 3\n #### #### Heading level 4\n ##### ##### Heading level 5\n ###### ###### Heading level 6',
-        id: '1234qwerty',
-    },
-    {
         customer: 'Styles',
         markdown:
             "I just love **&#42;&#42;bold text&#42;&#42;**. \n Italicized text is the *&#42;cat's meow&#42;*.",
@@ -26,12 +20,6 @@ const initialData = [
         markdown:
             '1. First item\n 2. Second item\n 3. Third item\n     1. First item\n     2. Second item\n 4. Fourth item',
         id: '5678asdfgh',
-    },
-    {
-        customer: 'Code',
-        markdown:
-            "# Code examples\n ### Standard \n```\nconst data = 'Lorem ipsum....';\n\nconst doSomething = (param) => {\n};\n\nconst xx = doSomething(data);\n```\n\n ### Javascript \n```js\nconst data = 'Lorem ipsum....';\n\nconst doSomething = (param) => {\n};\n\nconst xx = doSomething(data);\n```\n\n ### Shell \n```sh\n$ node index.js;\n```\n\n ### Java \n ```java\n String foo = 5;\n```",
-        id: '5278aswegh',
     },
 ];
 
