8bc3f3888c32fb2f559c0ac49aa812950cc2a155
Wilder González Díaz <wildergd@gmail.com>
2021-05-07 22:31:48 -0400

    feat: add loadingMessage prop to useMutationFlow hook (#379)
    * feat: add loadingMessage to useMutationFlow hook
    
    * Update useMutationFlow.story.mdx
    
    * Update useMutationFlow.story.mdx
    
    * Update index.js

--- packages/hooks/src/useMutationFlow/index.js
+++ packages/hooks/src/useMutationFlow/index.js
@@ -81,6 +81,7 @@ const resolveFeedbackActionFn = (type) => {
 const useMutationFlow = (opts) => {
     const {
         mutation = async () => {},
+        submitSpinnerMessage,
         successMessage = defaults.success.description,
         errorMessage = defaults.error.description,
         onSuccess = () => {},
@@ -91,7 +92,7 @@ const useMutationFlow = (opts) => {
 
     const mutate = useCallback(async (...args) => {
         hideAppMessage();
-        showAppSpinner();
+        showAppSpinner({ message: submitSpinnerMessage });
         try {
             const showAction = successMessage !== null;
             const res = await mutation(...args);
