93903583adc79980edc2b218efc26f450cc20213
David Rodriguez <drodriguezfeito@gmail.com>
2021-06-04 11:05:12 -0400

    fix: set default value on state

--- packages/hooks/src/useFloatingBarScrollHandler/index.ts
+++ packages/hooks/src/useFloatingBarScrollHandler/index.ts
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from 'react';
 import { Params, ReturnType } from './types';
 
 const useFloatingBarScrollHandler = ({ scrollTop = 50 }: Params): ReturnType => {
-    const [showFloatingBar, setShowFloatingBar] = useState<boolean>();
+    const [showFloatingBar, setShowFloatingBar] = useState<boolean>(false);
     const handleScrollEvent = useCallback(
         (event: React.UIEvent<HTMLElement>) => {
             if (!showFloatingBar && event.currentTarget.scrollTop > scrollTop) {
@@ -14,7 +14,7 @@ const useFloatingBarScrollHandler = ({ scrollTop = 50 }: Params): ReturnType => 
         [scrollTop, showFloatingBar],
     );
 
-    return [!!showFloatingBar, handleScrollEvent];
+    return [showFloatingBar, handleScrollEvent];
 };
 
 export default useFloatingBarScrollHandler;
