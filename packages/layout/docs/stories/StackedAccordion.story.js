bb978d08ffd1683fc189c148be2522b01fba530a
David Rodriguez <drodriguezfeito@gmail.com>
2021-06-16 01:39:38 -0400

    fix: update StackedAccordion example

--- packages/layout/docs/stories/StackedAccordion.story.js
+++ packages/layout/docs/stories/StackedAccordion.story.js
@@ -1,56 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Application } from 'react-rainbow-components';
+import { Application, Tree, Card, Accordion, AccordionSection } from 'react-rainbow-components';
 import StackedAccordion from '../../src/components/StackedAccordion';
 import StackedAccordionSection from '../../src/components/StackedAccordionSection';
+
+const treeData = [
+    {
+        label: 'sendWelcomeEmail',
+        isExpanded: true,
+        children: [{ label: 'Versions' }, { label: 'Overview' }],
+    },
+    { label: 'sendTransactionalEmail' },
+    { label: 'sendWelcomeEmail' },
+    {
+        label: 'sendWelcomeEmail',
+        isExpanded: true,
+        children: [{ label: 'Versions' }, { label: 'Overview' }],
+    },
+    { label: 'sendTransactionalEmail' },
+    { label: 'sendWelcomeEmail' },
+    {
+        label: 'sendTransactionalEmail',
+        isExpanded: true,
+        children: [{ label: 'Versions' }, { label: 'Overview' }],
+    },
+];
 
 const Container = styled.div`
     height: 350px;
     width: 100%;
+    display: flex;
+    flex-direction: row;
 `;
 
 export const BasicStackedAccordion = () => {
+    const [data, setData] = useState(treeData);
+    const [selectedNode, setSelectedNode] = useState();
+
+    const expandNode = ({ nodePath }) => {
+        const child = Tree.getNode(data, nodePath);
+        child.isExpanded = !child.isExpanded;
+        setData([...data]);
+    };
+
     return (
         <Application>
             <Container>
-                <StackedAccordion activeSectionNames={['section-1']}>
+                <StackedAccordion activeSectionNames={['section-1']} style={{ flex: 1 }}>
                     <StackedAccordionSection label="Private" name="section-1">
-                        <h1>Section 1 content</h1>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
+                        <Tree
+                            data={data}
+                            selectedNode={selectedNode}
+                            onNodeSelect={(node) => setSelectedNode(node.name)}
+                            onNodeExpand={expandNode}
+                        />
                     </StackedAccordionSection>
                     <StackedAccordionSection label="Public" name="section-2">
-                        Section 2 content
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
-                        <p>Lorem ipsum dolor sit amet</p>
+                        <Tree
+                            data={data}
+                            selectedNode={selectedNode}
+                            onNodeSelect={(node) => setSelectedNode(node.name)}
+                            onNodeExpand={expandNode}
+                        />
                     </StackedAccordionSection>
                     <StackedAccordionSection label="Other" name="section-3">
-                        Section 3 content
+                        <Tree
+                            data={data}
+                            selectedNode={selectedNode}
+                            onNodeSelect={(node) => setSelectedNode(node.name)}
+                            onNodeExpand={expandNode}
+                        />
                     </StackedAccordionSection>
                 </StackedAccordion>
+                <Card
+                    className="rainbow-p-horizontal_large rainbow-p-vertical_large"
+                    style={{ flex: 3 }}
+                    title="Version history"
+                >
+                    <Accordion>
+                        <AccordionSection label="Version 1.1.0">
+                            A rainbow is a meteorological phenomenon that is caused by reflection,
+                            refraction and dispersion of light in water droplets resulting in a
+                            spectrum of light appearing in the sky.
+                        </AccordionSection>
+                        <AccordionSection label="Version 1.0.1">
+                            A rainbow is a meteorological phenomenon that is caused by reflection,
+                            refraction and dispersion of light in water droplets resulting in a
+                            spectrum of light appearing in the sky.
+                        </AccordionSection>
+                        <AccordionSection label="Version 1.0.0">
+                            A rainbow is a meteorological phenomenon that is caused by reflection,
+                            refraction and dispersion of light in water droplets resulting in a
+                            spectrum of light appearing in the sky.
+                        </AccordionSection>
+                    </Accordion>
+                </Card>
             </Container>
         </Application>
     );
