e9cc71c50f3ce9820bd5e58a8e5977f2938f9d61
Jose Leandro Torres <jtorressicilia@gmail.com>
2021-07-21 17:03:45 -0400

    fix: cypress dist structure (#422)
    * fix: cypress package dist structure
    
    * chore: add packages/cypress/pageObjects to .eslintignore

--- packages/cypress/src/pageObjects/index.ts
+++ packages/cypress/src/pageObjects/index.ts
@@ -1,3 +1,5 @@
+export { default as Button } from './Button';
+export { default as Input } from './Input';
 export { default as InternalDropdown } from './InternalDropdown';
 export { default as Option } from './Option';
 export { default as Picklist } from './Picklist';
