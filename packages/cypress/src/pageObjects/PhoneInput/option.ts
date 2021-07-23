52da6936cb9de11eb618840cf1b40aa331f90d3e
Jose Leandro Torres <jtorressicilia@gmail.com>
2021-07-22 23:22:29 -0400

    fix: add PhoneInput page object (#424)

---
+++ packages/cypress/src/pageObjects/PhoneInput/option.ts
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+export interface ICountryOption {
+    click: () => void;
+}
+
+/**
+ * Page object to perform operations on a country option
+ * @type {ICountryOption}
+ */
+class CountryOption implements ICountryOption {
+    private rootElement: string;
+
+    /**
+     * Constructs a new instance of this class
+     * @param rootElement The root element for the page object.
+     */
+    constructor(rootElement: string) {
+        this.rootElement = rootElement;
+    }
+
+    /**
+     * Clicks the country option
+     */
+    click(): void {
+        cy.get(this.rootElement).scrollIntoView().click();
+    }
+}
+
+export default CountryOption;
