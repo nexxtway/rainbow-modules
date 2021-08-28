aa436224d2fde19d6ddabdf5868b0c4255ddc0d5
David Rodriguez <drodriguezfeito@gmail.com>
2021-08-28 17:06:19 -0400

    chore: add cypress components testing

---
+++ cypress/plugins/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devServer: {
+        allowedHosts: ['127.0.0.1'],
+    },
+};
