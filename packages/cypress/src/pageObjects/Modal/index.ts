7d4b87d69c30bbea8dd7413efb876c0826278db1
David Rodriguez Feitó <drodriguezfeito@gmail.com>
2021-09-11 22:26:54 -0400

    feat: implement Modal page object (#445)
    * feat: implement Modal page object
    
    * fix: change class name in docs
    
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

---
+++ packages/cypress/src/pageObjects/Modal/index.ts
@@ -0,0 +1,113 @@
+/// <reference types="cypress" />
+
+import { AssertMap } from '../types';
+
+/**
+ * @interface Chainer
+ * @template Subject
+ */
+interface Chainer {
+    /**
+     * Asserts if the title of the modal match the one provided
+     * @param visible {boolean} - Whether the app message should be visible or not
+     */
+    (chainer: 'title', title: string): void;
+    /**
+     * Asserts if the visibility of the modal match the one provided
+     * @param visible {boolean} - Whether the app message should be visible or not
+     */
+    (chainer: 'visible', visible: boolean): void;
+}
+
+export interface IModal {
+    header: Cypress.Chainable<JQuery<HTMLElement>>;
+    content: Cypress.Chainable<JQuery<HTMLElement>>;
+    footer: Cypress.Chainable<JQuery<HTMLElement>>;
+    close: () => void;
+    expect: Chainer;
+}
+
+/**
+ * The Modal page object allows to perform actions on the Modal component
+ * of `react-rainbow-components` library.
+ * @class
+ * @implements {IModal}
+ * @type {Modal}
+ */
+class Modal implements IModal {
+    private rootElement: string;
+
+    /**
+     * A map with all the assertions.
+     * @private
+     */
+    private assertMap: AssertMap = {
+        title: (_: string, title: string) => {
+            this.header.should('have.text', title);
+        },
+        visible: (_: string, visible: boolean) => {
+            if (visible) {
+                cy.get(this.rootElement).should('exist').should('be.visible');
+            } else {
+                cy.get(this.rootElement).should('not.exist');
+            }
+        },
+    };
+
+    /**
+     * Constructs a new instance of this page object
+     * @param rootElement The selector for the root element of the Input.
+     */
+    constructor(rootElement: string) {
+        this.rootElement = rootElement;
+    }
+
+    /**
+     * Get the root element wrapped in `cy`
+     * @private
+     */
+    private get root(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.rootElement);
+    }
+
+    /**
+     * Get the header of the modal
+     * @member {Cypress.Chainable<JQuery<HTMLElement>>}
+     */
+    get header(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return this.root.find('[id^="modal-heading-"]');
+    }
+
+    /**
+     * Get the footer of the modal
+     * @member {Cypress.Chainable<JQuery<HTMLElement>>}
+     */
+    get footer(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return this.root.find('section[role="dialog"] > footer');
+    }
+
+    /**
+     * Get the content of the modal
+     * @member {Cypress.Chainable<JQuery<HTMLElement>>}
+     */
+    get content(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return this.root.find('[id^="modal-content-"]');
+    }
+
+    /**
+     * Close this modal clicking the close button
+     * @method
+     */
+    close(): void {
+        this.root.find('#modal-close-button').click();
+    }
+
+    /**
+     * Execute an assertion on this page object
+     */
+    get expect(): Chainer {
+        return (chainer: string, ...params: any[]) => this.assertMap[chainer](chainer, ...params);
+    }
+}
+
+export default Modal;
