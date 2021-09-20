2072a21aaab343cfeae11074256750d42088c6b2
David Rodriguez <drodriguezfeito@gmail.com>
2021-09-20 19:55:01 -0400

    fix: add visibility assertion

--- packages/cypress/src/pageObjects/InputSearch/index.ts
+++ packages/cypress/src/pageObjects/InputSearch/index.ts
@@ -1,9 +1,20 @@
 /// <reference types="cypress" />
 
+import { AssertMap } from '../types';
+
+interface Chainer {
+    /**
+     * Asserts if the visibility of the search input match the provided value
+     * @param visible {boolean} - Whether the app message should be visible or not
+     */
+    (chainer: 'visible', visible: boolean): void;
+}
+
 export interface IInputSearch {
     input: Cypress.Chainable<JQuery<HTMLInputElement>>;
     clear: () => void;
     search: () => void;
+    expect: Chainer;
 }
 
 /**
@@ -15,6 +26,20 @@ export interface IInputSearch {
  */
 class InputSearch implements IInputSearch {
     private rootElement: string;
+
+    /**
+     * A map with all the assertions.
+     * @private
+     */
+    private assertMap: AssertMap = {
+        visible: (_: string, visible: boolean) => {
+            if (visible) {
+                this.input.should('be.visible');
+            } else {
+                this.input.should('not.be.visible');
+            }
+        },
+    };
 
     /**
      * Constructs a new instance of this page object.
@@ -44,6 +69,13 @@ class InputSearch implements IInputSearch {
      */
     search(): void {
         this.input.type('{enter}');
+    }
+
+    /**
+     * Execute an assertion on this page object
+     */
+    get expect(): Chainer {
+        return (chainer: string, ...params: any[]) => this.assertMap[chainer](chainer, ...params);
     }
 }
 
