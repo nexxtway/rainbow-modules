55766e6d8202fda887d94f34621b8e9021576d9d
Wilder Gonzalez Diaz <wildergd@gmail.com>
2021-10-11 12:44:20 -0400

    fix: lint issues

--- packages/app/src/components/AppSpinner/index.js
+++ packages/app/src/components/AppSpinner/index.js
@@ -23,10 +23,10 @@ const SpinnerContainer = styled.div`
     height: 100%;
     background-color: rgba(239, 241, 245, 0.9);
     z-index: ${ZINDEX_SPINNER};
+
     @supports ((-webkit-backdrop-filter: none) or (backdrop-filter: none)) {
         background-color: rgba(239, 241, 245, 0.8);
         backdrop-filter: blur(5px);
-        -webkit-backdrop-filter: blur(5px);
     }
 `;
 
