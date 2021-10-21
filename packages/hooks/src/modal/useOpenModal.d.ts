e8c2db2a1beda823fa1e13aa6539e494212f76e9
Jose Leandro Torres <jtorressicilia@gmail.com>
2021-10-21 10:52:01 -0400

    fix: useOpenModal return types (#476)

--- packages/hooks/src/modal/useOpenModal.d.ts
+++ packages/hooks/src/modal/useOpenModal.d.ts
@@ -1,3 +1,5 @@
-declare function useOpenModal(modalId: string): [];
+declare function useOpenModal(
+    modalId: string,
+): [(props?: Record<string, unknown>) => void, () => void];
 
 export default useOpenModal;
