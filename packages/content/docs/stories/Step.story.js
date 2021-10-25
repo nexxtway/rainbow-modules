d19de02900c8818b5cf13d69cbfaca749e09ce52
David Rodriguez <drodriguezfeito@gmail.com>
2021-10-25 14:44:20 -0400

    feat: implement Step page object

--- packages/content/docs/stories/Step.story.js
+++ packages/content/docs/stories/Step.story.js
@@ -6,10 +6,11 @@ import logo from '../../../../getting-started/assets/rainbow-modules-logo.svg';
 export const SimpleStep = () => {
     return (
         <Application>
-            <Step number={1} header="Step 1" description="This is the first step">
+            <Step id="step-1" number={1} header="Step 1" description="This is the first step">
                 <img src={logo} alt="rainbow-logo" />
             </Step>
             <Step
+                id="step-2"
                 number={2}
                 header="This is the second step. It comes after the first step, and before the third one."
                 description="This is the second step. It comes after the first one."
