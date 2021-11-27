bb0f01db7ffb6a1a87d04b4ad6faf2e719e9828b
LeandroTorresSicilia <jtorressicilia@gmail.com>
2021-11-27 17:06:37 -0500

    feat: implement useHttpAuthQuery

---
+++ packages/firebase-hooks/src/http/useHttpAuthQuery.ts
@@ -0,0 +1,28 @@
+import { useQuery, UseQueryResult, UseQueryOptions } from 'react-query';
+import useAuthFetch from './useAuthFetch';
+
+interface Params {
+    functionName: string;
+    pathname: string;
+    region?: string;
+    key?: string;
+}
+
+const useHttpAuthQuery = (
+    params: Params,
+    config: Omit<UseQueryOptions, 'queryKey' | 'queryFn'>,
+): UseQueryResult => {
+    const { functionName, region, pathname, key } = params;
+    const queryKey = key || [functionName, pathname];
+    const { fetch } = useAuthFetch({ functionName, region });
+    return useQuery(
+        queryKey,
+        () =>
+            fetch(pathname, {
+                method: 'GET',
+            }),
+        config,
+    );
+};
+
+export default useHttpAuthQuery;
