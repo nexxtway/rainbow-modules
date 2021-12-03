dd5c503c86a163de6c1979df7dbc44f1adf3044a
LeandroTorresSicilia <jtorressicilia@gmail.com>
2021-12-03 14:19:44 -0500

    fix: useHttpAuthMutation to throw an error when request not success

--- packages/firebase-hooks/docs/stories/useHttpAuthMutation.story.js
+++ packages/firebase-hooks/docs/stories/useHttpAuthMutation.story.js
@@ -3,6 +3,7 @@ import { RainbowFirebaseApp } from '@rainbow-modules/app';
 import { Button } from 'react-rainbow-components';
 import app from '../../../../firebase';
 import useHttpAuthMutation from '../../src/http/useHttpAuthMutation';
+import useMutationFlow from '../../../hooks/src/useMutationFlow';
 
 const Example = () => {
     const { mutate, isLoading } = useHttpAuthMutation({
@@ -29,6 +30,39 @@ export const UseHttpAuthMutation = () => {
     return (
         <RainbowFirebaseApp app={app}>
             <Example />
+        </RainbowFirebaseApp>
+    );
+};
+
+const ExampleWithMutationFlow = () => {
+    // Note that here you need to use mutateAsync in order to work with the useMutationFlow
+    const { mutateAsync, data, error } = useHttpAuthMutation({
+        functionName: 'expressTestFn',
+        pathname: '/customHelloWorld',
+        method: 'POST',
+    });
+    const { mutate } = useMutationFlow({
+        mutation: mutateAsync,
+    });
+
+    // eslint-disable-next-line no-console
+    const onMutate = () => mutate({ name: 'John Doe' }, { onError: (error) => console.log(error) });
+
+    return (
+        <>
+            <Button label="Mutate" onClick={onMutate} />
+            <div className="rainbow-m-top_medium">Response:</div>
+            <div>{JSON.stringify(data)}</div>
+            <div className="rainbow-m-top_medium">Error:</div>
+            <div>{JSON.stringify(error)}</div>
+        </>
+    );
+};
+
+export const UseHttpAuthMutationWithMutationFlow = () => {
+    return (
+        <RainbowFirebaseApp app={app}>
+            <ExampleWithMutationFlow />
         </RainbowFirebaseApp>
     );
 };
