36d58ee02f03219497b25aa869dad70b986364d8
Tahimi <tahimileon@gmail.com>
2022-01-08 14:01:28 -0500

    fix: table container styles (#514)
    * fix: table container styles
    
    * fix: styles

--- packages/listview/src/components/FirestoreTableWithCursors/styled.js
+++ packages/listview/src/components/FirestoreTableWithCursors/styled.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components';
+import Table from 'react-rainbow-components/components/Table';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    height: 100%;
+    box-sizing: border-box;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const StyledTable = styled(Table)`
+    overflow: auto;
+`;
 
 export const Footer = styled.footer`
     display: flex;
     justify-content: flex-end;
     width: 100%;
-    padding: 12px;
+    padding: 12px 12px 0 12px;
 `;
