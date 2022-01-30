ef07ff522f5ff3ac7bf4b651f8cac02d127866bc
amontalvof <amontalvof92@gmail.com>
2022-01-30 14:50:15 -0600

    feat: download modal component

---
+++ packages/listview/src/components/DownloadModal/helpers/__test__/convertToCSV.spec.js
@@ -0,0 +1,21 @@
+import convertToCSV from '../convertToCSV';
+
+describe('convertToCSV', () => {
+    it('should convert data to CSV', () => {
+        const data = [
+            {
+                name: 'Item 1',
+                color: { r: 0, g: 255, b: 0 },
+                size: 'X-Large',
+                max: '500',
+                format: 'csv',
+            },
+            { name: 'Item 2', color: 'Red', size: 'X-Medium', max: '500', format: 'csv' },
+            { name: 'Item 3', color: 'Blue', size: 'X-Small', max: '500', format: 'csv' },
+        ];
+        const result = convertToCSV(data);
+        expect(result).toBe(
+            'name,color,size,max,format\nItem 1,[object Object],X-Large,500,csv\nItem 2,Red,X-Medium,500,csv\nItem 3,Blue,X-Small,500,csv',
+        );
+    });
+});
--- packages/listview/src/index.js
+++ packages/listview/src/index.js
@@ -10,3 +10,4 @@ export { default as DraggableList } from './components/DraggableList';
 export { default as filterByFields } from './helpers/filterByFields';
 export { default as MarkdownColumn } from './components/MarkdownColumn';
 export { default as VirtualizedTable, useTableDataSource } from './components/VirtualizedTable';
+export { default as DownloadModal } from './components/DownloadModal';
