7487e434d279e3c4c7887bb5d377ab8d0e93619c
David Rodriguez Feito <drodriguezfeito@gmail.com>
2022-01-31 16:06:25 -0500

    fix: improve AuditLogs types

--- packages/listview/src/components/AuditLogs/types.ts
+++ packages/listview/src/components/AuditLogs/types.ts
@@ -1,10 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ReactNode, RefObject } from 'react';
 
+export type Severity = 'debug' | 'info' | 'warning' | 'error';
+
+export interface Filters {
+    severity?: Severity[];
+    dateRange?: DateRange;
+    labels?: Record<string, string[]>;
+}
+
 export interface AuditLogsProps {
     collectionPath: string;
     labels?: string[];
-    defaultFilter?: Record<string, any>;
+    defaultFilter?: Filters;
 }
 
 export interface SeverityProps {
@@ -24,8 +32,6 @@ export type ContextType = {
     updateFilters: (newFilters: Filters) => void;
 };
 
-export type Severity = 'debug' | 'info' | 'warning' | 'error';
-
 export type LabelFilter = {
     name: string;
     value: string;
@@ -41,12 +47,6 @@ export interface SeveritySelectProps {
 export interface DateRange {
     name?: 'all' | 'today' | 'this-week' | 'this-month' | 'custom';
     label?: string;
-}
-
-export interface Filters {
-    severity?: Severity[];
-    dateRange?: DateRange;
-    labels?: Record<string, string[]>;
 }
 
 export interface PicklistValue {
