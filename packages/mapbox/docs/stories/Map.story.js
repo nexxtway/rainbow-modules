4bc3e71eea51a7b5a3f02b7d6045d583221cac8d
Andy Montalvo <59586600+amontalvof@users.noreply.github.com>
2022-08-16 23:36:50 -0500

    fix: button icon margin (#594)
    * fix: button icon margin
    
    * fix: fix crash on editable ColoredStatusColumn
    
    * chore: trigger ci
    
    Co-authored-by: David Rodriguez <drodriguezf93@gmail.com>
    Co-authored-by: LeandroTorresSicilia <jtorressicilia@gmail.com>

--- packages/mapbox/docs/stories/Map.story.js
+++ packages/mapbox/docs/stories/Map.story.js
@@ -58,6 +58,7 @@ const getWaypoints = (origin, destination) => {
         }
         waypoints.push([destination.geometry.location.lng(), destination.geometry.location.lat()]);
     }
+
     return waypoints;
 };
 
