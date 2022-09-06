5f6db552f8dc70edaa83aee9b0bf4167a3736276
David Rodriguez Feitó <drodriguezf93@gmail.com>
2022-09-06 14:49:08 -0300

    docs: add some missing docstrings (#600)

--- packages/app/src/helpers/getReactRouterVersion.js
+++ packages/app/src/helpers/getReactRouterVersion.js
@@ -1,5 +1,10 @@
 import * as router from 'react-router-dom';
 
+/**
+ * Get the react-router-dom installed version.
+ *
+ * @returns The react-router-dom version being used as a string ('5' | '6')
+ */
 const getReactRouterVersion = () =>
     Object.prototype.hasOwnProperty.call(router, 'Switch') ? '5' : '6';
 
