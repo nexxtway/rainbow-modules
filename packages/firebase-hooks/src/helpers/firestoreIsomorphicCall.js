416df2c3142e2e35a618b00a8722cd26cc5ab4f9
David Rodriguez <drodriguezf93@gmail.com>
2022-09-06 14:45:45 -0300

    docs: add some missing docstrings

--- packages/firebase-hooks/src/helpers/firestoreIsomorphicCall.js
+++ packages/firebase-hooks/src/helpers/firestoreIsomorphicCall.js
@@ -1,6 +1,15 @@
 import * as firestore from 'firebase/firestore';
 import getCallableKey from './getCallableKey';
 
+/**
+ * Call a function on a Firebase ref regardless of the Firebase version
+ *
+ * @param ref - A firestore reference
+ * @param {*} keyV8 - The name of the function to call if using Firebase 8
+ * @param {*} keyV9 - The name of the function to call if using Firebase 9
+ * @param  {...any} params - The params to forward to the called function
+ * @returns A rejected Promise if something went wrong, and the result of calling the Firebase function in any other case.
+ */
 const firestoreIsomorphicCall = (ref, keyV8, keyV9, ...params) => {
     if (!ref) return Promise.reject(new Error('No reference provided'));
 
