ba3d431c5f857a01623ee6a4b481f171da6e7c6f
LeandroTorresSicilia <jtorressicilia@gmail.com>
2022-11-03 01:04:36 -0700

    fix: failing tests

--- packages/app/src/components/ConfirmModal/styled.js
+++ packages/app/src/components/ConfirmModal/styled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Button as RainbowButton } from 'react-rainbow-components';
 
 export const Container = styled.div`
     display: flex;
@@ -25,10 +24,4 @@ export const ButtonsContainer = styled.div`
     display: flex;
     justify-content: flex-end;
     padding: 4px 0 0 0;
-`;
-
-export const Button = styled(RainbowButton)`
-    padding-left: 1.5rem;
-    padding-right: 1.5rem;
-    margin-left: 0.7rem;
 `;
