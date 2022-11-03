042e3a1d57fd9345bf1b19b32cf548f9c76483ae
LeandroTorresSicilia <jtorressicilia@gmail.com>
2022-11-03 00:21:01 -0700

    feat: implement useReduxForm hook

--- packages/hooks/src/index.js
+++ packages/hooks/src/index.js
@@ -7,3 +7,4 @@ export { default as useOpenModal } from './modal/useOpenModal';
 export { default as useMutationFlow } from './useMutationFlow';
 export { default as useStorageState } from './useStorageState';
 export { default as usePrevious } from './usePrevious';
+export { default as useReduxForm } from './useReduxForm';
